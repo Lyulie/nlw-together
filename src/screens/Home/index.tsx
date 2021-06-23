1cbdcd3881380d5d916c4e8f9f12d5222481e36f
Rodolpho Alves <Lyulie@github.com>
2021-06-23 02:10:25 -0300

    iniciando a flatlist para home e seus componentes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { FlatList, View, Text } from "react-native";
 import { styles } from "./styles";
 import { Profile } from "../../components/Profile";
 import { ButtonAdd } from "../../components/ButtonAdd";
 import { CategorySelect } from "../../components/CategorySelect";
+import { HeaderList } from "../../components/HeaderList";
 
 export function Home() {
 
     const [category, setCategory] = useState('')
+
+    const appointments = [
+        {
+            id : '1',
+            guild : {
+                id : '1',
+                name : 'Lendários',
+                icon : null,
+                owner : true 
+            },
+            category : '1',
+            date : '23/06 às 01:48',
+            description : 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+        }
+    ]
 
     function handleCategorySelect(categoryId : string) {
         categoryId === category? setCategory('') : setCategory(categoryId)
@@ -24,6 +40,23 @@ export function Home() {
                 <CategorySelect 
                     categorySelected={category}
                     setCategory={handleCategorySelect}
+                />
+            </View>
+
+            <View style={styles.content}>
+                <HeaderList
+                    title="Partidas Agendadas"
+                    subtitle="Total 6"
+                />
+
+                <FlatList
+                    data={appointments}
+                    keyExtractor={item => item.id}
+                    renderItem={
+                        ({ item }) => (
+                            <Text>{item.guild.name}</Text>
+                        )
+                    }
                 />
             </View>
         </View>
